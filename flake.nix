24dac35b7dafe1ef196df1c2b1da5f748407e005
kirkham <jason.kirkham@gmail.com>
2024-12-21 16:17:05 -0500

    first commit

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Kirkham NiOS";
+
+  inputs = {
+    # NixOS official package source, using the nixos-24.11 branch here
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.backupFileExtension = "backup";
+          home-manager.users.kirkham = import ./home.nix;
+        }
+      ];
+    };
+  };
+}
---
+++ home.nix
@@ -0,0 +1,65 @@
+{ config, pkgs, ... }:
+{
+  imports = [
+    ./hyprland.nix
+  ];
+
+  home.username = "kirkham";
+  home.homeDirectory = "/home/kirkham";
+
+  # Let home-manager manage itself
+  programs.home-manager.enable = true;
+
+  # Packages that should be installed to the user profile
+  home.packages = with pkgs; [
+    # Terminals
+    kitty      # GPU-accelerated terminal emulator
+    alacritty  # Fast, GPU-accelerated terminal emulator
+
+    # Browsers
+    firefox    # Popular open-source web browser from Mozilla
+
+    # Development
+    git        # Distributed version control system
+    vscodium   # Open source build of VS Code
+    code-cursor      # AI-powered code editor with pair programming features
+
+    # File management and system tools
+    xfce.thunar # File manager
+    rofi-wayland # Application launcher for Wayland
+    libinput     # Library to handle input devices
+    hyprland-protocols
+    libinput-gestures
+    
+    # Terminal utilities
+    fastfetch   # Fast and feature-rich terminal file manager
+    pfetch     # Lightweight system information tool
+    btop       # Resource monitor that shows usage and stats
+    tree       # Directory listing command that produces a depth-indented listing
+
+  ];
+
+  # Git configuration
+  programs.git = {
+    enable = true;
+    userName = "kirkham";
+    userEmail = "jason.kirkham@gmail.com";
+    extraConfig = {
+      init.defaultBranch = "main";
+    };
+  };
+
+  # Shell configuration (zsh)
+  programs.zsh = {
+    enable = true;
+    enableAutosuggestions = true;
+    enableSyntaxHighlighting = true;
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # The stateVersion should match your initial NixOS installation version
+  home.stateVersion = "25.05";
+
+}
